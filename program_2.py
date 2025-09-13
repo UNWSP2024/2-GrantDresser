ffe9bb84c136097215cfcad7f5165f44fe3c2bb6
GrantDresser <gfdresser@students.unwsp.edu>
2025-09-12 23:43:34 -0500

    Week Two Assignment

--- program_2.py
+++ program_2.py
@@ -1,13 +1,19 @@
-
 def average_age():
     # Get User Input
+    age1 = int (input("Enter age of friend 1:16 "))
+    age2 = int (input("Enter age of friend 2:17 "))
+    age3 = int (input("Enter age of friend 3:19 "))
+    age4 = int (input("Enter age of friend 4:16 "))
+    age5 = int (input("Enter age of friend 5:18 "))
 
 
     # Sum ages
+    total_age = age1 + age2 + age3 + age4 + age5
 
     # Average the ages
-
+    average = total_age / 5
     # Print the results
+    print("The average age is:", average)
 
 # Line which calls the above function.
 average_age()
--- program_3.py
+++ program_3.py
@@ -1,8 +1,25 @@
 def calculate_total_purchase():
     # A customer in a store is purchasing five items.  
-    # Write a program that asks for each item, 
-    # then displays the subtotal of the sale, 
-    # the amount of sales tax, and the total.  
-    # Assume the sales tax is 7 percent.
+    item1 = float(input("Enter price of item 1:10.99 "))
+    item2 = float(input("Enter price of item 2:3.49 "))
+    item3 = float(input("Enter price of item 3:7.25 "))
+    item4 = float(input("Enter price of item 4:29.99 "))
+    item5 = float(input("Enter price of item 5:15.99 "))
+
+    # Calculate subtotal
+    subtotal = item1 + item2 + item3 + item4 + item5
+
+    # Calculate sales tax
+    sales_tax = subtotal * 0.07
+
+    # Calculate total
+    total = subtotal + sales_tax
+
+    # Display the results
+    print("Subtotal: $", subtotal)
+    print("Sales Tax: $", sales_tax)
+    print("Total: $", total)
+
+
 
 calculate_total_purchase()
